3d43f46dfc5f77cc54886b8b9cf48e48d997d3ea
Kitty Guo <yg1848@nyu.edu>
2024-02-08 23:01:07 -0500

    first commit

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//该脚本控制游戏的基本流程，规则，等等
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
